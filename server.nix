f3aff718106fcb9f1bfc01a840ec573ccb800c7f
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-02-11 11:23:38 -0500

    remove extra stuff

--- server.nix
+++ server.nix
@@ -4,14 +4,10 @@
     <nixpkgs/nixos/modules/virtualisation/google-compute-image.nix>
     ./proxycontainers.nix
   ];
-
-  services.openssh.enable = true;
   users.users.root.openssh.authorizedKeys.keyFiles = [ ~/.ssh/id_rsa.pub ];
 
   proxycontainers = {
     enable = true;
     containers = import ./containers.nix;
   };
-
-  networking.firewall.allowedTCPPorts = [ 22 443 ];
 }
