5a92788cdd10f93696661d8d98d5ce4c4c1a89b1
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-02-11 12:06:33 -0500

    import proxycontainers

--- server.nix
+++ server.nix
@@ -1,12 +1,21 @@
-{ ... }:
-{
+{ pkgs, ... }:
+let
+  proxycontainers = builtins.fetchGit {
+    url = "https://github.com/kylesferrazza/proxycontainers.git";
+    rev = "25b7c343dafef9047fb85b0343d3dcf84c1f2175";
+    ref = "master";
+  };
+in {
   imports = [
     <nixpkgs/nixos/modules/virtualisation/google-compute-image.nix>
-    ./proxycontainers.nix
+    "${proxycontainers}"
   ];
 
   proxycontainers = {
     enable = true;
     containers = import ./containers.nix;
+    sslServerCert = ../nix-gce/cert.pem;
+    sslServerChain = ../nix-gce/fullchain.pem;
+    sslServerKey = ../nix-gce/privkey.pem;
   };
 }
